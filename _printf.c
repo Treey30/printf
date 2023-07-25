31c3195a342de03997678f8f57acb13de7af8516
Treey30 <trizawayula@gmail.com>
2023-07-25 23:36:51 +0300

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,13 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "main.h"
+
+// Function prototypes
+void print_buffer(char buffer[], int *buff_ind);
+int handle_print(const char *format, int *index, va_list args, char buffer[],
+                 int flags, int width, int precision, int size);
+int get_flags(const char *format, int *index);
+int get_width(const char *format, int *index, va_list args);
+int get_precision(const char *format, int *index, va_list args);
+int get_size(const char *format, int *index);
 
 /**
  * _printf - Custom printf function to produce output according to the format.
@@ -8,76 +16,62 @@
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
-    va_start(args, format);
+    int i, printed_chars = 0, flags, width, precision, size, buff_ind = 0, printed;
+    va_list list;
+    char buffer[BUFF_SIZE];
 
-    while (*format)
+    if (format == NULL)
+        return (-1);
+
+    va_start(list, format);
+
+    for (i = 0; format && format[i] != '\0'; i++)
     {
-        if (*format != '%')
+        if (format[i] != '%')
         {
-            putchar(*format);
-            count++;
+            buffer[buff_ind++] = format[i];
+            if (buff_ind == BUFF_SIZE)
+                print_buffer(buffer, &buff_ind);
+            printed_chars++;
         }
         else
         {
-            format++; // Move past the '%'
-            switch (*format)
+            print_buffer(buffer, &buff_ind);
+            flags = get_flags(format, &i);
+            width = get_width(format, &i, list);
+            precision = get_precision(format, &i, list);
+            size = get_size(format, &i);
+            ++i;
+            printed = handle_print(format, &i, list, buffer,
+                                   flags, width, precision, size);
+            if (printed == -1)
             {
-                case 'c':
-                {
-                    char ch = va_arg(args, int);
-                    putchar(ch);
-                    count++;
-                    break;
-                }
-                case 's':
-                {
-                    char *str = va_arg(args, char *);
-                    if (str)
-                    {
-                        while (*str)
-                        {
-                            putchar(*str);
-                            str++;
-                            count++;
-                        }
-                    }
-                    break;
-                }
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-                default:
-                    // Invalid conversion specifier, just print the '%'
-                    putchar('%');
-                    putchar(*format);
-                    count += 2;
-                    break;
+                va_end(list);
+                return (-1);
             }
+            printed_chars += printed;
         }
-        format++;
     }
 
-    va_end(args);
-    return count;
+    print_buffer(buffer, &buff_ind);
+
+    va_end(list);
+
+    return (printed_chars);
 }
 
-int main()
+/**
+ * print_buffer - Prints the contents of the buffer if it exists.
+ * @buffer: Array of chars.
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
 {
-    int num = 42;
-    char ch = 'A';
-    char *str = "Hello, World!";
+    if (*buff_ind > 0)
+    {
+        write(1, &buffer[0], *buff_ind);
+    }
 
-    // Test the _printf function
-    int printed_chars = _printf("This is a number: %d\n", num);
-    printed_chars += _printf("This is a character: %c\n", ch);
-    printed_chars += _printf("This is a string: %s\n", str);
-    printed_chars += _printf("This is a percent sign: %%\n");
-
-    printf("Total characters printed: %d\n", printed_chars);
-
-    return 0;
+    *buff_ind = 0;
 }
 
